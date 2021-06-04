24189d0ac4de3ed6b805c0cf801f8d5ecfd5f6a9
venu-shastri <venu.shastri@gmail.com>
2021-06-04 10:48:56 +0530

    Task Cancellation Updated
    Code Updted

---
+++ TaskCancellation/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskCancellation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource _tokenSource = new CancellationTokenSource();
+            CancellationToken _token = _tokenSource.Token;
+            //  _tokenSource.Cancel();
+
+            Task _task = Task.Factory.StartNew(AsyncTask, _token, _token);
+
+            System.Threading.Thread.Sleep(5000);
+            _tokenSource.Cancel();
+            try
+            {
+                _task.Wait();
+            }
+            catch(AggregateException ex)
+            {
+                foreach( Exception _exception in ex.Flatten().InnerExceptions)
+                {
+                    Console.WriteLine(_exception.Message);
+                }
+            }
+
+            Console.WriteLine(_task.IsCanceled);
+        }
+
+        static void AsyncTask(object state)
+        {
+            CancellationToken _token = (CancellationToken)state;
+            
+
+            for (int i = 0; i < 10; i++)
+                {
+                //if (_token.IsCancellationRequested)
+                //{
+                //    return;
+                //}
+                // _token.ThrowIfCancellationRequested();
+                if (i == 5)
+                {
+                    if (_token.WaitHandle.WaitOne())
+                    {
+                        return;
+                    }
+                }
+                Console.WriteLine($"Async Task {i}");
+
+                    System.Threading.Thread.Sleep(1000);
+                }
+
+            
+        }
+    }
+
+}
+   
+    
+
